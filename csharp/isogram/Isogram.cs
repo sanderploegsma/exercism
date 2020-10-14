6ebe585254a3b62fb0713c3766852421579c0a79
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 13:26:13 +0200

    Add csharp/isogram

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) =>
+        word.ToLowerInvariant().ToCharArray()
+            .Where(c => Enumerable.Range('a', 26).Contains(c))
+            .GroupBy(c => c)
+            .Where(g => g.Count() > 1)
+            .Count() == 0;
+}

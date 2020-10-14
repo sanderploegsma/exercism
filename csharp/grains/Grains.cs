cf691d0f5e3c4a4b25936210ae808c2bca62d376
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 13:38:50 +0200

    Add csharp/grains

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    private const int Size = 64;
+
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > Size) {
+            throw new ArgumentOutOfRangeException($"Square should be between 1 and {Size}");
+        }
+
+        return Convert.ToUInt64(Math.Pow(2, n - 1));
+    }
+
+    public static ulong Total() => Enumerable.Range(1, Size).Select(Square).Aggregate(0UL, (a, b) => a + b);
+}

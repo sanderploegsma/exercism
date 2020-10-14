2e691c92d50598001d33d6f6643a85880bded439
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 14:21:24 +0200

    Add csharp/scrabble-score

---
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+
+public static class ScrabbleScore
+{
+    public static int Score(string input) => input.ToCharArray().Sum(Value);
+
+    private static int Value(char c)
+    {
+        switch (char.ToUpperInvariant(c))
+        {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                throw new ArgumentException("Invalid character");
+        }
+    }
+}

4abafbf36e255f230de8983491028b66904a1e70
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 13:15:44 +0200

    Add csharp/pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input) => 
+        Enumerable.Range('a', 26).All(c => input.ToLowerInvariant().Contains((char) c)); 
+}

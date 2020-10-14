db0f520978f201d0916e449e605176feaaa24958
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 13:56:09 +0200

    Add csharp/collatz-conjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException("Only positive numbers are allowed");
+        }
+
+        return Steps(number, 0);
+    }
+
+    private static int Steps(int number, int steps)
+    {
+        if (number == 1)
+        {
+            return steps;
+        }
+
+        if (number % 2 == 0)
+        {
+            return Steps(number / 2, steps + 1);
+        }
+
+        return Steps(3 * number + 1, steps + 1);
+    }
+}

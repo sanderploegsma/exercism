023852d808075b44c2a3551bcde413d884a36a1a
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 13:51:43 +0200

    Add csharp/perfect-numbers

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException($"{number} is not a natural number");
+        }
+
+        var factors = Factors(number);
+        var aliquotSum = factors.Where(f => f <= number / 2).Sum();
+        
+        if (aliquotSum == number)
+        {
+            return Classification.Perfect;
+        }
+
+        return aliquotSum < number ? Classification.Deficient : Classification.Abundant;
+    }
+
+    private static IEnumerable<int> Factors(int number) => Enumerable.Range(1, number).Where(i => number % i == 0);
+}

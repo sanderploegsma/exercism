2bf5eb0763641ed621527ec4a011ea91a1024775
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 15:02:38 +0200

    Add csharp/sum-of-multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max) =>
+        Enumerable.Range(1, max - 1)
+            .Where(a => multiples.Any(b => a.IsMultipleOf(b)))
+            .Sum();
+}
+
+internal static class Extensions
+{
+    public static bool IsMultipleOf(this int self, int other) => other switch
+    {
+        0 => false,
+        _ => self % other == 0
+    };
+}

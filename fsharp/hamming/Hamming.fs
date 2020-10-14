32de9da146ea42d9bf72287c4d6d6bdbf2f998bf
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-14 17:46:31 +0200

    fsharp/hamming

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,8 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+    if strand1.Length <> strand2.Length then None
+    else Seq.zip strand1 strand2
+        |> Seq.filter (fun (x, y) -> x <> y)
+        |> Seq.length
+        |> Some

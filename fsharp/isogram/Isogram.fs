e8e67264d36b2cff815690872cd46ca1a1912626
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 15:46:15 +0200

    fsharp/isogram

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,7 @@
+﻿module Isogram
+
+let isIsogram (str: string) = 
+    str.ToLowerInvariant()
+    |> Seq.filter "abcdefghijklmnopqrstuvwxyz".Contains
+    |> Seq.countBy (fun c -> c) 
+    |> Seq.forall (fun (c, x) -> x = 1)

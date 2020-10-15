f498e9444c460b417d8578c5152df64989a5158d
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 16:15:55 +0200

    fsharp/sum-of-multiples

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,9 @@
+﻿module SumOfMultiples
+
+let isMultipleOf x y = x % y = 0
+let isMultipleOfAny list x = List.exists (isMultipleOf x) list
+
+let sum (numbers: int list) (upperBound: int): int =
+    seq { 1 .. upperBound }
+    |> Seq.filter (isMultipleOfAny numbers)
+    |> Seq.sum

a506dfb33c9de5ea9d81c1eef569d9d6a4680100
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 15:53:15 +0200

    fsharp/rna-transcription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,15 @@
+﻿module RnaTranscription
+
+let complement (n: char) =
+    match n with
+    | 'G' -> Some 'C'
+    | 'C' -> Some 'G'
+    | 'T' -> Some 'A'
+    | 'A' -> Some 'U'
+    | _ -> None
+
+let toRna (dna: string): string =
+    dna
+    |> Seq.choose complement
+    |> Seq.toArray
+    |> System.String

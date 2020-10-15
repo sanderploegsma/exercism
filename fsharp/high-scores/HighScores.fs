6553e3f22901fa51f32a69e4f56e89b2f3fa2748
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 15:41:31 +0200

    fsharp/high-scores

---
+++ fsharp/high-scores/HighScores.fs
@@ -0,0 +1,9 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+
+let latest (values: int list): int = values |> List.last
+
+let personalBest (values: int list): int = values |> List.max
+
+let personalTopThree (values: int list): int list = values |> List.sortDescending |> List.truncate 3

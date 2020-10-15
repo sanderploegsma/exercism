848309319ff569af2df085b8855123adf991a149
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 16:00:25 +0200

    fsharp/difference-of-squares

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,9 @@
+﻿module DifferenceOfSquares
+
+let square n = pown n 2
+
+let squareOfSum (number: int): int = seq { 1 .. number } |> Seq.sum |> square
+
+let sumOfSquares (number: int): int = seq { 1 .. number } |> Seq.sumBy square
+
+let differenceOfSquares (number: int): int = (squareOfSum number) - (sumOfSquares number)

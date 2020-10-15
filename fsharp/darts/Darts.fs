22b97bc4a3b9210c1170210da4e6e9aae1c40a0d
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-15 15:34:46 +0200

    fsharp/darts

---
+++ fsharp/darts/Darts.fs
@@ -0,0 +1,9 @@
+﻿module Darts
+
+let score (x: double) (y: double): int =
+    let dist = x * x + y * y |> sqrt
+    match dist with
+    | z when z <= 1.0 -> 10
+    | z when z <= 5.0 -> 5
+    | z when z <= 10.0 -> 1
+    | _ -> 0

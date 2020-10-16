58996a972690c67884767a83e2af34d01d59fe2b
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-16 16:58:28 +0200

    Refactor python/pangram
    Use string.ascii_lowercase instead of manually providing the alphabet

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,7 @@
+from string import ascii_lowercase
+
 def is_pangram(sentence):
-    for c in "abcdefghijklmnopqrstuvwxyz":
+    for c in ascii_lowercase:
         if c not in sentence.lower():
             return False
     return True

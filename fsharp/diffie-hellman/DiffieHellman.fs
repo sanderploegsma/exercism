79a64abe7e25ad145e5ec8e8c331add1dee34e95
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-16 11:01:29 +0200

    fsharp/diffie-helman

---
+++ fsharp/diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,18 @@
+﻿module DiffieHellman
+
+open System
+open System.Numerics
+open System.Security.Cryptography
+
+let privateKey (primeP: bigint) =
+    let rng = new RNGCryptoServiceProvider()
+    let bytes = primeP.ToByteArray()
+    let mutable r = BigInteger(-1)
+    while r < 0I || r >= primeP do
+        rng.GetBytes(bytes)
+        r <- BigInteger(bytes)
+    r
+
+let publicKey primeP primeG privateKey = BigInteger.ModPow(primeG, privateKey, primeP)
+
+let secret primeP publicKey privateKey = BigInteger.ModPow(publicKey, privateKey, primeP)

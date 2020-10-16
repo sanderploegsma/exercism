ed24306296694b39dad045320a0cba61689343bc
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-16 11:49:23 +0200

    fsharp/dnd-character

---
+++ fsharp/dnd-character/DndCharacter.fs
@@ -0,0 +1,37 @@
+﻿module DndCharacter
+
+open System
+
+let modifier x = 
+    let calc a = (a - 10.0) / 2.0
+    x |> float |> calc |> floor |> int
+
+let ability() = 
+    let rnd = Random()
+    List.init 4 (fun _ -> rnd.Next(1, 6)) 
+    |> List.sortDescending 
+    |> List.take 3 
+    |> List.sum
+
+type Character = {
+    Strength: int;
+    Dexterity: int;
+    Constitution: int;
+    Intelligence: int;
+    Wisdom: int;
+    Charisma: int;
+    Hitpoints: int;
+}
+
+let createCharacter() =
+    let constitution = ability()
+    let hp = 10 + modifier constitution
+    { 
+        Strength=ability(); 
+        Dexterity=ability(); 
+        Constitution=constitution; 
+        Intelligence=ability(); 
+        Wisdom=ability(); 
+        Charisma=ability(); 
+        Hitpoints=hp
+    }

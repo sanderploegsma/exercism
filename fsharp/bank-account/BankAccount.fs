21c28a5fb91091fefd178f828bca7b81369cd79a
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-16 15:26:07 +0200

    fsharp/bank-account

---
+++ fsharp/bank-account/BankAccount.fs
@@ -0,0 +1,28 @@
+﻿module BankAccount
+
+open System
+open System.Collections.Concurrent
+
+let store = ConcurrentDictionary<Guid, Decimal>()
+
+let mkBankAccount() = Guid.NewGuid()
+
+let openAccount account =
+    store.AddOrUpdate(account, 0m, (fun _ _ -> 0m)) |> ignore
+    account
+
+let closeAccount account =
+    store.TryRemove(account) |> ignore
+    account
+
+let getBalance account =
+    let mutable balance = 0m
+    if store.TryGetValue(account, &balance) then
+        Some balance
+    else
+        None
+
+let updateBalance change account =
+    if store.ContainsKey(account) then
+        store.AddOrUpdate(account, change, (fun _ balance -> balance + change)) |> ignore
+    account

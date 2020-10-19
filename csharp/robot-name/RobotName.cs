f48451e87d0ed052d02579b6bb3b8429c475d2ee
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-19 13:35:48 +0200

    Refactor csharp/robot-name after feedback
    - Use do/while loop to avoid calling GenerateName() twice
    - Fix never-ending loop when all possible names are in use

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,40 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Robot
 {
-    private string _name;
+    public string Name { get; private set; }
 
-    public Robot() => _name = RobotNameGenerator.GenerateUniqueName();
+    public Robot() => Name = RobotNameGenerator.GenerateUniqueName();
 
-    public string Name => _name;
-
-    public void Reset() => _name = RobotNameGenerator.GenerateUniqueName();
+    public void Reset() => Name = RobotNameGenerator.GenerateUniqueName();
 }
 
-internal static class RobotNameGenerator {
+internal static class RobotNameGenerator
+{
     private const string Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private const string Numeric = "0123456789";
+
+    private static readonly string[] Recipe = { Alpha, Alpha, Numeric, Numeric, Numeric };
+    private static readonly int NumberOfUniqueNames = Recipe.Aggregate(1, (acc, seq) => acc * seq.Length);
 
     private static readonly ISet<string> UsedNames = new HashSet<string>();
 
     public static string GenerateUniqueName()
     {
-        var name = GenerateName();
-        while (!UsedNames.Add(name))
+        if (UsedNames.Count == NumberOfUniqueNames)
+        {
+            throw new Exception("Number of unique names exhausted");
+        }
+
+        string name;
+        do
         {
             name = GenerateName();
-        }
+        } while (!UsedNames.Add(name));
+
         return name;
     }
 
     private static string GenerateName()
     {
         var rand = new Random();
+        char RandomCharacter(string sequence) => sequence[rand.Next(sequence.Length - 1)];
 
-        char RandomAlpha() => Alpha[rand.Next(Alpha.Length - 1)];
-        char RandomNumeric() => Numeric[rand.Next(Numeric.Length - 1)];
-
-        return string.Join("", RandomAlpha(), RandomAlpha(), RandomNumeric(), RandomNumeric(), RandomNumeric());
+        return string.Join("", Recipe.Select(RandomCharacter));
     }
 }

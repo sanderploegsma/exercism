5d19ac78a2f79fd2bb50fc83a44aa6817163e406
Sander Ploegsma <sanderploegsma@gmail.com>
2020-10-21 14:14:22 +0200

    python/hangman

---
+++ python/hangman/hangman.py
@@ -0,0 +1,36 @@
+# Game status categories
+# Change the values as you see fit
+STATUS_WIN = "win"
+STATUS_LOSE = "lose"
+STATUS_ONGOING = "ongoing"
+
+
+class Hangman:
+    def __init__(self, word):
+        self.remaining_guesses = 9
+        self.status = STATUS_ONGOING
+        self.word = word
+        self.guesses = []
+
+    def guess(self, char):
+        if self.status != STATUS_ONGOING:
+            raise ValueError("Game already over")
+        if self.remaining_guesses < 0:
+            raise ValueError("No guesses left")
+        
+        if char in self.guesses or char not in self.word:
+            self.remaining_guesses -= 1
+            
+        self.guesses.append(char)
+
+        if self.get_masked_word() == self.word:
+            self.status = STATUS_WIN
+        
+        if self.remaining_guesses < 0:
+            self.status = STATUS_LOSE
+
+    def get_masked_word(self):
+        return "".join([c if c in self.guesses else '_' for c in self.word])
+
+    def get_status(self):
+        return self.status

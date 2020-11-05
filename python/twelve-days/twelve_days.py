97a64422e22846f0418748554e0a12a2c7606806
Sander Ploegsma <sanderploegsma@gmail.com>
2020-11-05 15:13:54 +0100

    [Python] Twelve Days

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,48 @@
+nth = [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+]
+
+items = [
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming",
+]
+
+
+def build(n: int) -> str:
+    item = items[n - 1]
+    if n == 1:
+        return f"{item}."
+
+    if n == 2:
+        return f"{item}, and " + build(n - 1)
+
+    return f"{item}, " + build(n - 1)
+
+
+def verse(n: int) -> list[str]:
+    return f"On the {nth[n - 1]} day of Christmas my true love gave to me: {build(n)}"
+
+
+def recite(start_verse: int, end_verse: int) -> list[list[str]]:
+    return [verse(n) for n in range(start_verse, end_verse + 1)]

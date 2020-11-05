0cc6b0faf079c95d908141247309c40f052e528c
Sander Ploegsma <sanderploegsma@gmail.com>
2020-11-05 16:30:52 +0100

    [C#] Flatten Array

---
+++ csharp/flatten-array/FlattenArray.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Linq;
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input) =>
+        input.Cast<object>()
+            .SelectMany(obj => obj switch
+            {
+                null => Enumerable.Empty<object>(),
+                IEnumerable enumerable => Flatten(enumerable).Cast<object>(),
+                _ => new[] {obj}
+            });
+}

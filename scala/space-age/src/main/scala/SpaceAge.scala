cf5803cb9435873e2cc566cb41e03d79322c33de
Sander Ploegsma <sanderploegsma@gmail.com>
2020-12-21 11:42:15 +0100

    Update SpaceAge after feedback

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,10 +1,35 @@
+sealed trait Planet
+case object Earth extends Planet
+case object Mercury extends Planet
+case object Venus extends Planet
+case object Mars extends Planet
+case object Jupiter extends Planet
+case object Saturn extends Planet
+case object Uranus extends Planet
+case object Neptune extends Planet
+
 object SpaceAge {
-  def onEarth(seconds: Double): Double = seconds / 31557600D
-  def onMercury(seconds: Double): Double = onEarth(seconds) / 0.2408467
-  def onVenus(seconds: Double): Double = onEarth(seconds) / 0.61519726
-  def onMars(seconds: Double): Double = onEarth(seconds) / 1.8808158
-  def onJupiter(seconds: Double): Double = onEarth(seconds) / 11.862615
-  def onSaturn(seconds: Double): Double = onEarth(seconds) / 29.447498
-  def onUranus(seconds: Double): Double = onEarth(seconds) / 84.016846
-  def onNeptune(seconds: Double): Double = onEarth(seconds) / 164.79132
+  private val earthOrbitalPeriodSeconds = 31557600D
+  private val relativeOrbitalPeriods: Map[Planet, Double] = Map(
+    Earth -> 1.0,
+    Mercury -> 0.2408467,
+    Venus -> 0.61519726,
+    Mars -> 1.8808158,
+    Jupiter -> 11.862615,
+    Saturn -> 29.447498,
+    Uranus -> 84.016846,
+    Neptune -> 164.79132
+  )
+
+  private def calculateAgeOn(planet: Planet): Double => Double =
+    seconds => seconds / earthOrbitalPeriodSeconds / relativeOrbitalPeriods.getOrElse(planet, 1.0)
+
+  def onEarth(seconds: Double): Double = calculateAgeOn(Earth).apply(seconds)
+  def onMercury(seconds: Double): Double = calculateAgeOn(Mercury).apply(seconds)
+  def onVenus(seconds: Double): Double = calculateAgeOn(Venus).apply(seconds)
+  def onMars(seconds: Double): Double = calculateAgeOn(Mars).apply(seconds)
+  def onJupiter(seconds: Double): Double = calculateAgeOn(Jupiter).apply(seconds)
+  def onSaturn(seconds: Double): Double = calculateAgeOn(Saturn).apply(seconds)
+  def onUranus(seconds: Double): Double = calculateAgeOn(Uranus).apply(seconds)
+  def onNeptune(seconds: Double): Double = calculateAgeOn(Neptune).apply(seconds)
 }

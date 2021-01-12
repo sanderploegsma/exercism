e3128810a9bc961cf09619d14cc1d7a626c341b6
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-12 14:49:08 +0100

    [Scala] Palindrome Products

--- scala/palindrome-products/src/main/scala/PalindromeProducts.scala
+++ scala/palindrome-products/src/main/scala/PalindromeProducts.scala
@@ -3,20 +3,21 @@ case class PalindromeProducts(lower: Int, upper: Int) {
     for {
       (x, i1) <- (lower to upper).zipWithIndex
       (y, i2) <- (lower to upper).zipWithIndex
-      if i1 <= i2
+      if i1 <= i2 && isPalindrome(x * y)
     } yield (x, y)
 
   private val palindromes =
     factors
-      .filter { case (x, y) => isPalindrome(x * y) }
       .groupBy { case (x, y) => x * y }
       .map { case (product, factors) => (product, factors.toSet) }
       .toSeq
       .sortBy(_._1)
 
-  def smallest: Option[(Int, Set[(Int, Int)])] = palindromes.headOption
+  def smallest: Option[(Int, Set[(Int, Int)])] =
+    palindromes.headOption
 
-  def largest: Option[(Int, Set[(Int, Int)])] = palindromes.lastOption
+  def largest: Option[(Int, Set[(Int, Int)])] =
+    palindromes.lastOption
 
   private def isPalindrome(i: Int): Boolean = {
     val digits = i.toString

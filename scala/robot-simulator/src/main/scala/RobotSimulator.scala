d2b18a5f35da4fdcfa80922599e06c85c2f08ce7
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-22 09:58:51 +0100

    Update after feedback

--- scala/robot-simulator/src/main/scala/RobotSimulator.scala
+++ scala/robot-simulator/src/main/scala/RobotSimulator.scala
@@ -1,27 +1,17 @@
 object Bearing extends Enumeration {
   val North, East, South, West = Value
-
-  implicit class BearingValue(bearing: Value) {
-    def turnLeft: Bearing.Value = bearing match {
-      case Bearing.North => Bearing.West
-      case Bearing.West => Bearing.South
-      case Bearing.South => Bearing.East
-      case Bearing.East => Bearing.North
-    }
-
-    def turnRight: Bearing.Value = bearing match {
-      case Bearing.North => Bearing.East
-      case Bearing.East => Bearing.South
-      case Bearing.South => Bearing.West
-      case Bearing.West => Bearing.North
-    }
-  }
 }
 
 case class Robot(bearing: Bearing.Value, coordinates: (Int, Int)) {
-  def turnLeft: Robot = copy(bearing = bearing.turnLeft)
+  def turnLeft: Robot = {
+    val bearings = Seq(Bearing.North, Bearing.West, Bearing.South, Bearing.East)
+    copy(bearing = nextBearing(bearings))
+  }
 
-  def turnRight: Robot = copy(bearing = bearing.turnRight)
+  def turnRight: Robot = {
+    val bearings = Seq(Bearing.North, Bearing.East, Bearing.South, Bearing.West)
+    copy(bearing = nextBearing(bearings))
+  }
 
   def advance: Robot = {
     val (x, y) = coordinates
@@ -36,6 +26,9 @@ case class Robot(bearing: Bearing.Value, coordinates: (Int, Int)) {
 
   def simulate(instructions: String): Robot =
     instructions.foldLeft(this)(_.simulate(_))
+
+  private def nextBearing(bearings: Seq[Bearing.Value]): Bearing.Value =
+    bearings.dropWhile(_ != bearing).drop(1).headOption.getOrElse(bearings.head)
 
   private def simulate(instruction: Char): Robot = instruction match {
     case 'A' => advance

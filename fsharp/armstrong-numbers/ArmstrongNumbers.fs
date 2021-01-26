9df739c3ccc410f508bb9999cc58c9761f792c82
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 16:54:50 +0100

    [F#] Armstrong Numbers

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,12 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool =
+    let digits =
+        number
+        |> string
+        |> Seq.map (System.Char.GetNumericValue >> int)
+
+    let digitsRaised =
+        Seq.map (fun d -> pown d (Seq.length digits)) digits
+
+    number = Seq.sum digitsRaised
---
+++ fsharp/armstrong-numbers/ArmstrongNumbersTests.fs
@@ -0,0 +1,45 @@
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+
+module ArmstrongNumbersTests
+
+open FsUnit.Xunit
+open Xunit
+
+open ArmstrongNumbers
+
+[<Fact>]
+let ``Zero is an Armstrong number`` () =
+    isArmstrongNumber 0 |> should equal true
+
+[<Fact>]
+let ``Single digit numbers are Armstrong numbers`` () =
+    isArmstrongNumber 5 |> should equal true
+
+[<Fact>]
+let ``There are no 2 digit Armstrong numbers`` () =
+    isArmstrongNumber 10 |> should equal false
+
+[<Fact>]
+let ``Three digit number that is an Armstrong number`` () =
+    isArmstrongNumber 153 |> should equal true
+
+[<Fact>]
+let ``Three digit number that is not an Armstrong number`` () =
+    isArmstrongNumber 100 |> should equal false
+
+[<Fact>]
+let ``Four digit number that is an Armstrong number`` () =
+    isArmstrongNumber 9474 |> should equal true
+
+[<Fact>]
+let ``Four digit number that is not an Armstrong number`` () =
+    isArmstrongNumber 9475 |> should equal false
+
+[<Fact>]
+let ``Seven digit number that is an Armstrong number`` () =
+    isArmstrongNumber 9926315 |> should equal true
+
+[<Fact>]
+let ``Seven digit number that is not an Armstrong number`` () =
+    isArmstrongNumber 9926314 |> should equal false
+

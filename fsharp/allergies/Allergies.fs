94e8124f2acdf219a26c99930a464bd761e945db
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 16:21:26 +0100

    [F#] Allergies

---
+++ fsharp/allergies/Allergies.fs
@@ -0,0 +1,21 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo (codedAllergies: int) (allergen: Allergen) = codedAllergies &&& (int allergen) <> 0
+
+let list (codedAllergies: int) =
+    // https://stackoverflow.com/a/50320841
+    (Enum.GetValues(typeof<Allergen>) :?> (Allergen []))
+    |> Array.filter (allergicTo codedAllergies)
+    |> Array.toList

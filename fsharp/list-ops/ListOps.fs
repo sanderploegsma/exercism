98a580c679c724cc61b1451da49d2f9194bf06d1
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 18:45:35 +0100

    [F#] List Ops

---
+++ fsharp/list-ops/ListOps.fs
@@ -0,0 +1,28 @@
+﻿module ListOps
+
+let rec foldl folder state list =
+    match list with
+    | [] -> state
+    | head :: tail -> foldl folder (folder state head) tail
+
+let rec foldr folder state list =
+    match list with
+    | [] -> state
+    | head :: tail -> folder head (foldr folder state tail)
+
+let length list =
+    list |> foldl (fun count _ -> count + 1) 0
+
+let reverse list =
+    list |> foldl (fun tail x -> x :: tail) []
+
+let map f list =
+    list |> foldr (fun x tail -> f x :: tail) []
+
+let filter f list =
+    list
+    |> foldr (fun x tail -> if f x then x :: tail else tail) []
+
+let append xs ys = foldr (fun x tail -> x :: tail) ys xs
+
+let concat xs = xs |> foldr append []

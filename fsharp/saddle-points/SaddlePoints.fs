8623119f572d483adedd57fb730f316c73ac873a
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 18:08:53 +0100

    [F#] Saddle Points (update)
    Rewrite using Array2D. Inspiration from https://exercism.io/tracks/fsharp/exercises/saddle-points/solutions/7be297831a1143f68d9260ebeb058c3d

--- fsharp/saddle-points/SaddlePoints.fs
+++ fsharp/saddle-points/SaddlePoints.fs
@@ -1,18 +1,14 @@
 module SaddlePoints
 
-let saddlePoints (matrix: int list list) =
-    let maxInRow = lazy (matrix |> List.map List.max)
-
-    let minInCol =
-        lazy (matrix |> List.transpose |> List.map List.min)
+let saddlePoints matrix =
+    let m = array2D matrix
 
     let isSaddlePoint row col =
-        let value = matrix.[row].[col]
+        let value = Array2D.get m row col
+        (m.[*, col] |> Array.forall ((<=) value)) &&
+        (m.[row, *] |> Array.forall ((>=) value))
 
-        value = maxInRow.Force().[row]
-        && value = minInCol.Force().[col]
-
-    [ for row in 0 .. List.length matrix - 1 do
-          for col in 0 .. List.length (List.head matrix) - 1 do
+    [ for row in 0 .. Array2D.length1 m - 1 do
+          for col in 0 .. Array2D.length2 m - 1 do
               if isSaddlePoint row col then
                   yield row + 1, col + 1 ]

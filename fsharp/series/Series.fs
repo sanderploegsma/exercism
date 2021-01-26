678adfef5ec4bb5de1f7e8a7d30b232d779e96b1
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 17:34:46 +0100

    [F#] Series

---
+++ fsharp/series/Series.fs
@@ -0,0 +1,13 @@
+﻿module Series
+
+let slices (str: string) length =
+    let create l = 
+        str
+        |> Seq.windowed l
+        |> Seq.map System.String.Concat
+        |> Seq.toList
+
+    Some length
+    |> Option.filter (fun l -> l > 0)
+    |> Option.filter (fun l -> l <= str.Length)
+    |> Option.map create

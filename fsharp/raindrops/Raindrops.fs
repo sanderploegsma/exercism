0c09322ae76887b29d19aeeda72889b8fb258311
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 15:16:45 +0100

    [F#] Raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,9 @@
+﻿module Raindrops
+
+let convert (number: int) =
+    [(3, "Pling"); (5, "Plang"); (7, "Plong")]
+    |> List.filter (fun (d, _) -> number % d = 0)
+    |> List.map snd
+    |> function
+       | [] -> string number
+       | sounds -> String.concat "" sounds

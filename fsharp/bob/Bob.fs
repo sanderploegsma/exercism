32a76b7159dfbe90aa83f70de20e32589f697471
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 16:06:08 +0100

    [F#] Bob

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,22 @@
+﻿module Bob
+
+let (|Question|_|) (s: string) =
+    if s.EndsWith('?') then
+        Some Question
+    else
+        None
+
+let (|Yelling|_|) (s: string) =
+    if s.ToUpperInvariant() = s
+       && String.exists System.Char.IsLetter s then
+        Some Yelling
+    else
+        None
+
+let response (input: string): string =
+    match input.Trim() with
+    | Yelling & Question -> "Calm down, I know what I'm doing!"
+    | Yelling -> "Whoa, chill out!"
+    | Question -> "Sure."
+    | "" -> "Fine. Be that way!"
+    | _ -> "Whatever."

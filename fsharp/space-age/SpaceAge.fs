f011d6cfe8ce2575ed53fb45b7322bd81d602093
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-26 15:26:27 +0100

    [F#] Space Age

---
+++ fsharp/space-age/SpaceAge.fs
@@ -0,0 +1,23 @@
+﻿module SpaceAge
+
+type Planet = 
+    | Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+let age (planet: Planet) (seconds: int64): float =
+    let earthYears = (float seconds) / 31557600.0
+    match planet with
+    | Earth -> earthYears
+    | Mercury -> earthYears / 0.2408467
+    | Venus -> earthYears / 0.61519726
+    | Mars -> earthYears / 1.8808158
+    | Jupiter -> earthYears / 11.862615
+    | Saturn -> earthYears / 29.447498
+    | Uranus -> earthYears / 84.016846
+    | Neptune -> earthYears / 164.79132

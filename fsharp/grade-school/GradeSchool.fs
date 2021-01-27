e99c32764b199066c0adeae52c242097b14cb1f4
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 13:36:40 +0100

    [F#} Grade School

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,26 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    let existing =
+        Map.tryFind grade school |> Option.defaultValue []
+
+    Map.add grade (student :: existing) school
+
+let roster (school: School): string list =
+    let flatten (grade, students) =
+        students
+        |> List.map (fun student -> grade, student)
+
+    Map.toList school
+    |> List.collect flatten
+    |> List.sortBy id
+    |> List.map snd
+
+let grade (number: int) (school: School): string list =
+    Map.tryFind number school
+    |> Option.defaultValue []
+    |> List.sort

83f71f3cbf593c266a8aebb319dfba64d8eb7d07
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 10:00:17 +0100

    [F#] Perfect Numbers

---
+++ fsharp/perfect-numbers/PerfectNumbers.fs
@@ -0,0 +1,23 @@
+﻿module PerfectNumbers
+
+type Classification =
+    | Perfect
+    | Abundant
+    | Deficient
+
+let classify n =
+    let factors x =
+        [ for i in 1 .. x / 2 do
+              if x % i = 0 then yield i ]
+
+    let aliquotSum x = List.sum (factors x)
+
+    let classification =
+        function
+        | sum when sum < n -> Deficient
+        | sum when sum > n -> Abundant
+        | _ -> Perfect
+
+    Some n
+    |> Option.filter (fun x -> x > 0)
+    |> Option.map (aliquotSum >> classification)

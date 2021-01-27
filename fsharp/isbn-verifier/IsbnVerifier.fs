ddd3b109a7b7b75a63afde26d7f718e2d6ba36a4
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 17:25:41 +0100

    [F#] ISBN Verifier

---
+++ fsharp/isbn-verifier/IsbnVerifier.fs
@@ -0,0 +1,25 @@
+﻿module IsbnVerifier
+
+open System.Text.RegularExpressions
+
+let parse (isbn: string) =
+    match isbn.Replace("-", "") with
+    | str when Regex.IsMatch(str, @"^\d{9}[\dX]$") -> Ok str
+    | _ -> Error "invalid format"
+
+let checksum isbn =
+    let value c =
+        match c with
+        | 'X' -> 10
+        | d -> System.Char.GetNumericValue(d) |> int
+
+    isbn
+    |> Seq.rev
+    |> Seq.mapi (fun i c -> (value c) * (i + 1))
+    |> Seq.sum
+    |> fun x -> x % 11
+
+let isValid isbn =
+    match parse isbn with
+    | Ok valid -> checksum valid = 0
+    | Error _ -> false

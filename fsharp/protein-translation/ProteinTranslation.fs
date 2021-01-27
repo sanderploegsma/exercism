415c9b24c542d45ad8a0d6d43b85c2f0b3c151fa
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 14:45:15 +0100

    [F#] Protein Translation

---
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -0,0 +1,30 @@
+﻿module ProteinTranslation
+
+let translate =
+    function
+    | "AUG" -> Some "Methionine"
+    | "UUU"
+    | "UUC" -> Some "Phenylalanine"
+    | "UUA"
+    | "UUG" -> Some "Leucine"
+    | "UCU"
+    | "UCC"
+    | "UCA"
+    | "UCG" -> Some "Serine"
+    | "UAU"
+    | "UAC" -> Some "Tyrosine"
+    | "UGU"
+    | "UGC" -> Some "Cysteine"
+    | "UGG" -> Some "Tryptophan"
+    | "UAA"
+    | "UAG"
+    | "UGA" -> None
+    | codon -> invalidArg codon "Unknown codon"
+
+let proteins rna =
+    Seq.chunkBySize 3 rna
+    |> Seq.map System.String
+    |> Seq.map translate
+    |> Seq.takeWhile Option.isSome
+    |> Seq.choose id
+    |> Seq.toList

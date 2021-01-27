a2bcd8336558b5bdb87017e4ee228b77669d915c
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 14:22:02 +0100

    [F#] Error Handling

---
+++ fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,25 @@
+﻿module ErrorHandling
+
+let handleErrorByThrowingException () = failwith "Here's your exception"
+
+let handleErrorByReturningOption (input: string) =
+    let mutable value = 0
+
+    if System.Int32.TryParse(input, &value) then
+        Some value
+    else
+        None
+
+let handleErrorByReturningResult (input: string) =
+    let mutable value = 0
+
+    if System.Int32.TryParse(input, &value) then
+        Ok value
+    else
+        Error "Could not convert input to integer"
+
+let bind = Result.bind
+
+let cleanupDisposablesWhenThrowingException resource =
+    use r = resource
+    failwith "Oh no!"

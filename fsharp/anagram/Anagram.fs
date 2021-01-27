60135b577d87a9b9bc672ffe7f1516cca3c0b632
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 10:33:26 +0100

    [F#] Anagram

---
+++ fsharp/anagram/Anagram.fs
@@ -0,0 +1,10 @@
+﻿module Anagram
+
+let findAnagrams (sources: string list) (target: string) =
+    let chars str = Seq.sort str |> Seq.toArray
+
+    let isAnagram (candidate: string) =
+        let a, b = candidate.ToLower(), target.ToLower()
+        a <> b && chars a = chars b
+
+    List.filter isAnagram sources

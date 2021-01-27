39e585fb9d984943a93f2db89cd6865711830e94
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 11:20:15 +0100

    [F#] ETL

---
+++ fsharp/etl/Etl.fs
@@ -0,0 +1,13 @@
+﻿module Etl
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> =
+    let transpose (score, letters) =
+        Seq.map (fun letter -> letter, score) letters
+
+    let lowercase (letter, score) = System.Char.ToLower letter, score
+
+    scoresWithLetters
+    |> Map.toSeq
+    |> Seq.collect transpose
+    |> Seq.map lowercase
+    |> Map.ofSeq

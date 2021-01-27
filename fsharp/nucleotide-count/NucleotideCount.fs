2e47f62e211ab4f9f25926755350d0036b895e6b
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 11:12:47 +0100

    [F#] Nucleotide Count

---
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -0,0 +1,18 @@
+﻿module NucleotideCount
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+    let nucleotides = [ 'A'; 'C'; 'G'; 'T' ]
+
+    let isNucleotide c = Seq.contains c nucleotides
+
+    let countNucleotides str =
+        let count n =
+            str |> Seq.filter (fun c -> n = c) |> Seq.length
+
+        nucleotides
+        |> Seq.map (fun n -> n, count n)
+        |> Map.ofSeq
+
+    Some strand
+    |> Option.filter (Seq.forall isNucleotide)
+    |> Option.map countNucleotides

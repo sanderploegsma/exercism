a882371a84fc7dc7b200eaacc0e550e38de7eb95
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-27 17:46:12 +0100

    [F#] Roman Numerals

---
+++ fsharp/roman-numerals/RomanNumerals.fs
@@ -0,0 +1,25 @@
+﻿module RomanNumerals
+
+let roman arabicNumeral =
+    let conversions =
+        [ ("IIIII", "V")
+          ("IIII", "IV")
+          ("VV", "X")
+          ("VIV", "IX")
+          ("XXXXX", "L")
+          ("XXXX", "XL")
+          ("LL", "C")
+          ("LXL", "XC")
+          ("CCCCC", "D")
+          ("CCCC", "CD")
+          ("DD", "M")
+          ("DCD", "CM") ]
+
+    let replace (str: string) (oldValue: string, newValue: string) = 
+        str.Replace(oldValue, newValue)
+
+    let convert numeral = List.fold replace numeral conversions
+
+    Seq.replicate arabicNumeral 'I'
+    |> System.String.Concat
+    |> convert

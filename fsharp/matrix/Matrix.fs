6fac2179a672d96a01ca9606abf173551f999220
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-28 16:01:43 +0100

    [F#] Matrix

---
+++ fsharp/matrix/Matrix.fs
@@ -0,0 +1,23 @@
+﻿module Matrix
+
+let private parse (matrix: string) =
+    matrix.Split("\n")
+    |> Array.map (fun s -> s.Split(" "))
+    |> array2D
+    |> Array2D.map int
+
+// https://stackoverflow.com/a/2369690/1595197
+let private flatten (values: 'a[,]) = 
+    values
+    |> Seq.cast<'a>
+    |> Seq.toList
+
+let row index matrix =
+    parse matrix
+    |> fun m -> m.[index - 1 .. index - 1, *]
+    |> flatten
+
+let column index matrix =
+    parse matrix
+    |> fun m -> m.[*, index - 1 .. index - 1]
+    |> flatten

bfc0eaccafd6959485f82d28a09d4d6ef050a18f
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-28 15:49:46 +0100

    [F#] Kindergarten Garden

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,39 @@
+﻿module KindergartenGarden
+
+type Plant =
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
+
+let students =
+    [ "Alice"
+      "Bob"
+      "Charlie"
+      "David"
+      "Eve"
+      "Fred"
+      "Ginny"
+      "Harriet"
+      "Ileana"
+      "Joseph"
+      "Kincaid"
+      "Larry" ]
+
+let toPlant =
+    function
+    | 'G' -> Grass
+    | 'C' -> Clover
+    | 'R' -> Radishes
+    | 'V' -> Violets
+    | n -> failwithf "unknown plant '%c'" n
+
+let plants (diagram: string) student =
+    diagram.Split("\n")
+    |> Array.toSeq
+    |> Seq.map (Seq.chunkBySize 2)
+    |> Seq.collect (Seq.zip students)
+    |> Seq.filter (fun (s, _) -> s = student)
+    |> Seq.collect snd
+    |> Seq.map toPlant
+    |> Seq.toList

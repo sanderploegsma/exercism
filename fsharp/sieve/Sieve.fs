96947e3f4e7dcf6fff8a416ef56ed3a5cd87079f
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-28 13:26:14 +0100

    [F#] Sieve

---
+++ fsharp/sieve/Sieve.fs
@@ -0,0 +1,12 @@
+﻿module Sieve
+
+let primes limit =
+    let withoutMultiplesOf p numbers = 
+        List.except [ p .. p .. limit ] numbers
+
+    let rec find numbers primes =
+        match numbers with
+        | [] -> primes
+        | p :: tail -> find (withoutMultiplesOf p tail) (primes @ [ p ])
+
+    find [ 2 .. limit ] []

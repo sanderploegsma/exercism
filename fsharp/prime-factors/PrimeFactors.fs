4fedf672ba6b8711856998f9196f806a180b9564
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-28 16:09:33 +0100

    [F#] Prime Factors

---
+++ fsharp/prime-factors/PrimeFactors.fs
@@ -0,0 +1,10 @@
+﻿module PrimeFactors
+
+let factors number =
+    let rec find n p primes =
+        match n with
+        | 1L -> primes
+        | n when n % (int64 p) = 0L -> find (n / (int64 p)) p (primes @ [ p ])
+        | n -> find n (p + 1) primes
+
+    find number 2 []

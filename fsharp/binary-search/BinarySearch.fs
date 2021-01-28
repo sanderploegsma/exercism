b8822f131364087eb9d8f403dc96e2e51937defb
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-28 11:19:13 +0100

    [F#] Binary Search

---
+++ fsharp/binary-search/BinarySearch.fs
@@ -0,0 +1,17 @@
+﻿module BinarySearch
+
+let find input value =
+    let rec search lst =
+        let left, right =
+            match List.length lst with
+            | 0 -> [], []
+            | 1 -> [], lst
+            | n -> List.splitAt (n / 2 - 1) lst
+
+        match left, right with
+        | _, (i, x) :: _ when x = value -> Some i
+        | l, (_, x) :: _ when x > value -> search l
+        | _, (_, x) :: r when x < value -> search r
+        | _ -> None
+
+    input |> Array.toList |> List.indexed |> search

d58c3c054d5e83c67346fe7bf892c8e73f685aa1
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-29 13:29:01 +0100

    [F#] Word Count

---
+++ fsharp/word-count/WordCount.fs
@@ -0,0 +1,9 @@
+﻿module WordCount
+
+open System.Text.RegularExpressions
+
+let countWords (phrase: string) =
+    let pattern = @"\w+'\w+|\w+"
+    [for m in Regex.Matches(phrase, pattern) -> m.Value.ToLower()]
+    |> List.countBy id
+    |> Map.ofList

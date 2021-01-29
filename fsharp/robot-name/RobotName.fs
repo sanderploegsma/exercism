1cc3ae7a619da6c64e63fe4d3140cc5b8df3711a
Sander Ploegsma <sanderploegsma@gmail.com>
2021-01-29 12:07:31 +0100

    [F#] Robot Name

---
+++ fsharp/robot-name/RobotName.fs
@@ -0,0 +1,33 @@
+﻿module RobotName
+
+open System
+
+let mutable usedNames = []
+
+let generate () =
+    let rnd = Random()
+    let randomChar min max = rnd.Next(int min, int max) |> char
+
+    let name =
+        Seq.initInfinite
+            (fun _ ->
+                String.Concat [ randomChar 'A' 'Z'
+                                randomChar 'A' 'Z'
+                                randomChar '0' '9'
+                                randomChar '0' '9'
+                                randomChar '0' '9' ])
+        |> Seq.skipWhile (fun name -> List.contains name usedNames)
+        |> Seq.head
+
+    usedNames <- name :: usedNames
+    name
+
+type Robot = { Name: string }
+
+let mkRobot () = { Name = generate () }
+
+let name robot = robot.Name
+
+let reset robot = 
+    usedNames <- List.except [robot.Name] usedNames
+    { robot with Name = generate () }

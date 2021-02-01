0167bb4bdc91a87fe0c38b921f6d0b27693307a5
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-01 17:11:30 +0100

    [C#] Simple Linked List

---
+++ csharp/simple-linked-list/SimpleLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SimpleLinkedList<T> : IEnumerable<T>
+{
+    public SimpleLinkedList(T value)
+    {
+        Value = value;
+    }
+
+    public SimpleLinkedList(IEnumerable<T> values)
+    {
+        Value = values.FirstOrDefault();
+        values.Skip(1).ToList().ForEach(value => Add(value));
+    }
+
+    public T Value { get; }
+
+    public SimpleLinkedList<T> Next { get; private set; }
+
+    public SimpleLinkedList<T> Add(T value)
+    {
+        if (Next == null)
+        {
+            Next = new SimpleLinkedList<T>(value);
+        }
+        else
+        {
+            Next.Add(value);
+        }
+
+        return this;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var instance = this;
+
+        while (instance != null)
+        {
+            yield return instance.Value;
+            instance = instance.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}

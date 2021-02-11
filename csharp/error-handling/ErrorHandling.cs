35afacfef63ba8396de26c317fe392893eebbe71
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 13:45:58 +0100

    [C#] Error Handling

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() => 
+        throw new Exception("Hey, you asked for it.");
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (int.TryParse(input, out var value))
+        {
+            return value;
+        }
+
+        return null;
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) => 
+        int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using var resource = disposableObject;
+        throw new Exception("Hi, I'm an error.");
+    }
+}

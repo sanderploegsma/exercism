68a10178e27ce9663e21abe1526fb59037127d83
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 11:32:23 +0100

    [C#] Darts

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var radius = Math.Sqrt(x * x + y * y);
+
+        if (radius <= 1.0)
+            return 10;
+        if (radius <= 5.0)
+            return 5;
+        if (radius <= 10.0)
+            return 1;
+        return 0;
+    }
+}

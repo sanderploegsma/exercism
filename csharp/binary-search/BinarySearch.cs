423c043d8d1f1ca11b02022ca234e6fcbc2a9733
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 10:43:43 +0100

    [C#] Binary Search

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,23 @@
+﻿public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        var lo = 0;
+        var hi = input.Length - 1;
+        
+        while (lo <= hi)
+        {
+            var mid = (lo + hi) / 2;
+
+            if (input[mid] == value)
+                return mid;
+
+            if (input[mid] < value)
+                lo = mid + 1;
+            else
+                hi = mid - 1;
+        }
+
+        return -1;
+    }
+}

1fe5b8e71b96ede68c4ddd32d6af5e9a0da88852
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 13:41:28 +0100

    [C#] Proverb

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        if (subjects.Length == 0)
+        {
+            return Array.Empty<string>();
+        }
+
+        return subjects
+            .Pairwise()
+            .Select(pieces => $"For want of a {pieces.Item1} the {pieces.Item2} was lost.")
+            .Append($"And all for the want of a {subjects.First()}.")
+            .ToArray();
+    }
+}
+
+internal static class EnumerableExtensions
+{
+    public static IEnumerable<Tuple<T, T>> Pairwise<T>(this IEnumerable<T> items)
+    {
+        var i = 0;
+        T previous = default;
+
+        foreach (var item in items)
+        {
+            if (i > 0)
+            {
+                yield return new Tuple<T, T>(previous, item);
+            }
+
+            previous = item;
+            i++;
+        }
+    }
+}

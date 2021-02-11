74b0f56bc9d1bbdc3d724b3ab3b4467e1053bde1
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 11:00:14 +0100

    [C#] Reverse String

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Text;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var builder = new StringBuilder();
+        
+        for (var i = input.Length - 1; i >= 0; i--)
+        {
+            builder = builder.Append(input[i]);
+        }
+
+        return builder.ToString();
+    }
+
+    // This would be my preferred method, but I'm sure using a built-in Reverse() function
+    // is not what this exercise is about
+    public static string ReverseLinq(string input) => 
+        string.Concat(input.Reverse());
+}

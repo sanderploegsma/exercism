a1c3b961921afbbf1d5c1c74d5f79c189f651fdf
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 14:25:16 +0100

    [C#] Kindergarten Garden

---
+++ csharp/kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Plant
+{
+    Violets,
+    Radishes,
+    Clover,
+    Grass
+}
+
+public class KindergartenGarden
+{
+    private readonly IDictionary<string, IEnumerable<Plant>> _garden;
+
+    private static readonly string[] Students =
+    {
+        "Alice",
+        "Bob",
+        "Charlie",
+        "David",
+        "Eve",
+        "Fred",
+        "Ginny",
+        "Harriet",
+        "Ileana",
+        "Joseph",
+        "Kincaid",
+        "Larry"
+    };
+
+    public KindergartenGarden(string diagram) => _garden = ParseDiagram(diagram);
+
+    public IEnumerable<Plant> Plants(string student) => _garden[student];
+
+    private static IDictionary<string, IEnumerable<Plant>> ParseDiagram(string diagram)
+    {
+        var rows = diagram.Split("\n");
+        var dict = new Dictionary<string, IEnumerable<Plant>>();
+
+        for (var i = 0; i < Students.Length; i++)
+        {
+            var cup = i * 2;
+            if (cup < rows[0].Length)
+            {
+                dict[Students[i]] =
+                    new[] {rows[0][cup], rows[0][cup + 1], rows[1][cup], rows[1][cup + 1]}.Select(ToPlant);
+            }
+        }
+
+        return dict;
+    }
+
+    private static Plant ToPlant(char plant) => plant switch
+    {
+        'V' => Plant.Violets,
+        'R' => Plant.Radishes,
+        'C' => Plant.Clover,
+        'G' => Plant.Grass,
+        _ => throw new ArgumentException($"Unknown plant {plant}")
+    };
+}

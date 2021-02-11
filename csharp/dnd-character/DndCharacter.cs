73f5c9855206c897f2e08d504061aa69dedaa17a
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-11 11:49:03 +0100

    [C#] D&D Character

---
+++ csharp/dnd-character/DndCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class DndCharacter
+{
+    private static readonly Random Rng = new Random();
+
+    public int Strength { get; private set; }
+    public int Dexterity { get; private set; }
+    public int Constitution { get; private set; }
+    public int Intelligence { get; private set; }
+    public int Wisdom { get; private set; }
+    public int Charisma { get; private set; }
+    public int Hitpoints => 10 + Modifier(Constitution);
+
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2d);
+
+    public static int Ability() =>
+        new[] {Rng.Next(1, 7), Rng.Next(1, 7), Rng.Next(1, 7), Rng.Next(1, 7)}
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+    public static DndCharacter Generate() => new DndCharacter
+    {
+        Strength = Ability(),
+        Dexterity = Ability(),
+        Constitution = Ability(),
+        Intelligence = Ability(),
+        Wisdom = Ability(),
+        Charisma = Ability()
+    };
+}

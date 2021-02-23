366a144512b3c2f2612dbb872514d85448eaccd7
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-23 15:20:51 +0100

    [C#] Rest API

---
+++ csharp/rest-api/Domain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestApi.Domain
+{
+    /// <summary>
+    /// Summary of a user.
+    /// </summary>
+    internal class UserSummary
+    {
+        public UserSummary(string user, IDictionary<string, decimal> balances)
+        {
+            User = user;
+            Balances = balances;
+        }
+
+        /// <summary>
+        /// The user this summary belongs to.
+        /// </summary>
+        public string User { get; }
+
+        /// <summary>
+        /// Amounts this user owes or is owed by other users.
+        /// </summary>
+        public IDictionary<string, decimal> Balances { get; }
+    }
+
+    internal class TransactionService
+    {
+        private readonly List<string> _users;
+        private readonly List<Transaction> _transactions;
+
+        public TransactionService()
+        {
+            _users = new List<string>();
+            _transactions = new List<Transaction>();
+        }
+
+        /// <summary>
+        /// Retrieve a summary for all users, or the filtered users if provided.
+        /// </summary>
+        /// <param name="filter">A list of users to return a summary for</param>
+        /// <returns>A summary for all users, or the filtered users if provided</returns>
+        public IEnumerable<UserSummary> GetUserSummaries(IReadOnlyCollection<string> filter = default) =>
+            from user in _users
+            where (filter ?? Enumerable.Empty<string>()).Contains(user)
+            orderby user
+            select GetSummaryForUser(user);
+
+        /// <summary>
+        /// Add a new user. It is not possible to add a user that already exists.
+        /// </summary>
+        /// <param name="user">Name of the user</param>
+        /// <returns>The summary of the created user</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the user already exists</exception>
+        public UserSummary AddUser(string user)
+        {
+            if (_users.Contains(user))
+            {
+                throw new InvalidOperationException("User already exists");
+            }
+
+            _users.Add(user);
+            return GetSummaryForUser(user);
+        }
+
+        /// <summary>
+        /// Add a new transaction between two users.
+        /// </summary>
+        /// <param name="lender">The user that lends the amount</param>
+        /// <param name="borrower">The user that borrows the amount</param>
+        /// <param name="amount">The amount involved in the transaction</param>
+        /// <returns>The user summary of both users</returns>
+        /// <exception cref="InvalidOperationException">Thrown when it is not possible to add a transaction for the given users</exception>
+        public (UserSummary Lender, UserSummary Borrower) AddTransaction(string lender, string borrower, decimal amount)
+        {
+            if (lender == borrower)
+            {
+                throw new InvalidOperationException("Cannot owe yourself");
+            }
+
+            if (!_users.Contains(lender))
+            {
+                throw new InvalidOperationException("Lender does not exist");
+            }
+
+            if (!_users.Contains(borrower))
+            {
+                throw new InvalidOperationException("Borrower does not exist");
+            }
+
+            _transactions.Add(new Transaction(lender, borrower, amount));
+            return (GetSummaryForUser(lender), GetSummaryForUser(borrower));
+        }
+
+        private UserSummary GetSummaryForUser(string user)
+        {
+            var balances = _transactions
+                .Where(r => r.Lender == user || r.Borrower == user)
+                .Select(r => r.Lender == user ? (r.Borrower, r.Amount) : (r.Lender, -r.Amount))
+                .GroupBy(x => x.Item1)
+                .ToDictionary(g => g.Key, g => g.Sum(x => x.Item2));
+
+            return new UserSummary(user, balances);
+        }
+
+        private class Transaction
+        {
+            public string Lender { get; }
+            public string Borrower { get; }
+            public decimal Amount { get; }
+
+            public Transaction(string lender, string borrower, decimal amount)
+            {
+                Lender = lender;
+                Borrower = borrower;
+                Amount = amount;
+            }
+        }
+    }
+}
---
+++ csharp/rest-api/RestApi.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using RestApi.Domain;
+
+namespace RestApi.Api
+{
+    public class RestApi
+    {
+        private readonly TransactionService _transactionService;
+
+        public RestApi(string database)
+        {
+            _transactionService = new TransactionService();
+
+            var data = JsonHelper.Deserialize<IEnumerable<UserSummaryResponse>>(database);
+            foreach (var item in data)
+            {
+                _transactionService.AddUser(item.Name);
+                foreach (var (borrower, amount) in item.OwedBy)
+                {
+                    _transactionService.AddTransaction(item.Name, borrower, amount);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Perform a GET request at the given URL
+        /// </summary>
+        /// <param name="url">Path of the request</param>
+        /// <param name="payload">Optional JSON payload of the request</param>
+        /// <returns>JSON response</returns>
+        public string Get(string url, string payload = null)
+        {
+            object response = url switch
+            {
+                "/users" => GetUsers(payload),
+                _ => throw new ArgumentException($"Unknown url {url}", nameof(url))
+            };
+
+            return JsonHelper.Serialize(response);
+        }
+
+        /// <summary>
+        /// Perform a POST request at the given URL with the given payload
+        /// </summary>
+        /// <param name="url">Path of the request</param>
+        /// <param name="payload">JSON payload of the request</param>
+        /// <returns>JSON response</returns>
+        public string Post(string url, string payload)
+        {
+            object response = url switch
+            {
+                "/add" => AddUser(payload),
+                "/iou" => AddTransaction(payload),
+                _ => throw new ArgumentException($"Unknown url {url}", nameof(url))
+            };
+
+            return JsonHelper.Serialize(response);
+        }
+
+        /// <summary>
+        /// Retrieve a list of users.
+        /// </summary>
+        /// <param name="payload">Optional JSON payload containing a list of users to return</param>
+        /// <returns>Summary of the users provided in the payload, or all users when no payload</returns>
+        private IEnumerable<UserSummaryResponse> GetUsers(string payload = null)
+        {
+            if (payload == null)
+            {
+                return _transactionService.GetUserSummaries().Select(ToUserSummaryResponse);
+            }
+
+            var request = JsonHelper.Deserialize<GetUsersRequest>(payload);
+            return _transactionService
+                .GetUserSummaries(request.Users)
+                .Select(ToUserSummaryResponse);
+        }
+
+        /// <summary>
+        /// Add a new user.
+        /// </summary>
+        /// <param name="payload">JSON payload containing the name of the user</param>
+        /// <returns>The summary of the created user</returns>
+        /// <exception cref="ArgumentException">Thrown when the payload is empty</exception>
+        private UserSummaryResponse AddUser(string payload)
+        {
+            if (string.IsNullOrWhiteSpace(payload))
+            {
+                throw new ArgumentException("Empty payload");
+            }
+
+            var request = JsonHelper.Deserialize<AddUserRequest>(payload);
+            var summary = _transactionService.AddUser(request.User);
+            return ToUserSummaryResponse(summary);
+        }
+
+        /// <summary>
+        /// Add a new transaction between two users. 
+        /// </summary>
+        /// <param name="payload">JSON payload containing the two users and an amount</param>
+        /// <returns>The summary for both users after the transaction is completed</returns>
+        /// <exception cref="ArgumentException">Thrown when the payload is empty</exception>
+        private IEnumerable<UserSummaryResponse> AddTransaction(string payload)
+        {
+            if (string.IsNullOrWhiteSpace(payload))
+            {
+                throw new ArgumentException("Empty payload");
+            }
+
+            var request = JsonHelper.Deserialize<AddTransactionRequest>(payload);
+            var result = _transactionService.AddTransaction(request.Lender, request.Borrower, request.Amount);
+            return new[] {result.Lender, result.Borrower}
+                .OrderBy(x => x.User)
+                .Select(ToUserSummaryResponse);
+        }
+
+        private static UserSummaryResponse ToUserSummaryResponse(UserSummary summary) => new UserSummaryResponse
+        {
+            Name = summary.User,
+            Balance = summary.Balances.Sum(x => x.Value),
+            Owes = summary.Balances
+                .Where(x => x.Value < 0)
+                .ToImmutableSortedDictionary(x => x.Key, x => -x.Value),
+            OwedBy = summary.Balances
+                .Where(x => x.Value > 0)
+                .ToImmutableSortedDictionary(x => x.Key, x => x.Value),
+        };
+    }
+
+    internal class UserSummaryResponse
+    {
+        public string Name { get; set; }
+
+        public IDictionary<string, decimal> Owes { get; set; }
+
+        public IDictionary<string, decimal> OwedBy { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+
+    internal class GetUsersRequest
+    {
+        public List<string> Users { get; set; }
+    }
+
+    internal class AddUserRequest
+    {
+        public string User { get; set; }
+    }
+
+    internal class AddTransactionRequest
+    {
+        public string Lender { get; set; }
+
+        public string Borrower { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+
+    internal static class JsonHelper
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new SnakeCaseNamingStrategy()
+            }
+        };
+
+        public static string Serialize<T>(T obj) => JsonConvert.SerializeObject(obj, JsonSerializerSettings);
+
+        public static T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json, JsonSerializerSettings);
+    }
+}

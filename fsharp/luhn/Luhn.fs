be09c384f01962f58dbc761a16eb7ecf03d5ef7e
Sander Ploegsma <sanderploegsma@gmail.com>
2021-02-24 09:55:20 +0100

    [F#] Luhn

---
+++ fsharp/luhn/Luhn.fs
@@ -0,0 +1,23 @@
+﻿module Luhn
+
+open System
+
+let valid (number: string) =
+    let double x =
+        match x + x with
+        | y when y > 9 -> y - 9
+        | y -> y
+
+    let validate n =
+        let checksum =
+            Seq.rev n
+            |> Seq.map (Char.GetNumericValue >> int)
+            |> Seq.mapi (fun i x -> if i % 2 = 1 then double x else x)
+            |> Seq.sum
+
+        checksum % 10 = 0
+
+    match number.Replace(" ", "") with
+    | n when String.length n < 2 -> false
+    | n when not (Seq.forall Char.IsDigit n) -> false
+    | n -> validate n

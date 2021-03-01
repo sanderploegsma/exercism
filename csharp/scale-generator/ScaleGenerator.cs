208b931c992609840a70f5f203b9f61f98a12277
Sander Ploegsma <sanderploegsma@gmail.com>
2021-03-01 15:21:56 +0100

    [C#] Scale Generator

---
+++ csharp/scale-generator/ScaleGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ScaleGenerator
+{
+    private static readonly string[] ScaleSharps =
+        {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+
+    private static readonly string[] ScaleFlats =
+        {"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"};
+
+    private static readonly string[] TonicsUsingFlats =
+        {"F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"};
+
+    public static string[] Chromatic(string tonic)
+    {
+        var scale = TonicsUsingFlats.Contains(tonic) ? ScaleFlats : ScaleSharps;
+        return scale.Concat(scale)
+            .SkipWhile(note => !string.Equals(note, tonic, StringComparison.CurrentCultureIgnoreCase))
+            .Take(12)
+            .ToArray();
+    }
+
+    public static string[] Interval(string tonic, string pattern)
+    {
+        var scale = Chromatic(tonic);
+
+        var result = new List<string>();
+        var i = 0;
+        foreach (var interval in pattern.Select(GetInterval))
+        {
+            result.Add(scale[i]);
+            i = (i + interval) % scale.Length;
+        }
+
+        return result.ToArray();
+    }
+
+    private static int GetInterval(char interval) => interval switch
+    {
+        'A' => 3,
+        'M' => 2,
+        'm' => 1,
+        _ => throw new ArgumentException()
+    };
+}

331948cc25138e7283ea36894dfbaa43544a5c57
Sander Ploegsma <sanderploegsma@gmail.com>
2021-03-01 10:57:06 +0100

    [C#] Nth Prime

---
+++ csharp/nth-prime/NthPrime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NthPrime
+{
+    public static int Prime(int nth)
+    {
+        if (nth < 1)
+            throw new ArgumentOutOfRangeException(nameof(nth), nth, "Parameter should be a positive number");
+
+        return Primes().Skip(nth - 1).First();
+    }
+
+    private static IEnumerable<int> Primes()
+    {
+        var primes = new HashSet<int>();
+        
+        return Enumerable
+            .Range(2, int.MaxValue - 2)
+            .Where(IsPrime)
+            .Where(primes.Add);
+
+        bool IsPrime(int n) => primes.All(x => n % x != 0);
+    }
+}

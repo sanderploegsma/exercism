f4a3f0bcd27f770f4268ff01dd49c1b5fed399dc
Sander Ploegsma <sanderploegsma@gmail.com>
2021-03-04 17:23:25 +0100

    [C#] Transpose

---
+++ csharp/transpose/Transpose.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Text;
+
+public static class Transpose
+{
+    public static string String(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return "";
+
+        var lines = input.Split('\n');
+        var width = lines.Max(line => line.Length);
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < lines.Length; j++)
+            {
+                var line = lines[j];
+                var shouldPad = lines.Skip(j + 1).Any(x => x.Length > i);
+                if (line.Length > i)
+                    sb.Append(line[i]);
+                else if (shouldPad)
+                    sb.Append(' ');
+            }
+
+            if (i < width - 1)
+                sb.Append('\n');
+        }
+
+        return sb.ToString();
+    }
+}

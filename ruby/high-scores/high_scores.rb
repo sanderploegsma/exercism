755049456ceaf6560713a549b638ad4ccf7610a6
Sander Ploegsma <sanderploegsma@gmail.com>
2021-06-30 10:04:07 +0200

    [Ruby] High Scores v2

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,22 +1,20 @@
 class HighScores
+  attr_reader :scores
+
   def initialize(scores)
     @scores = scores
   end
   
-  def scores
-    @scores
-  end
-  
   def latest
-    @scores[-1]
+    scores.last
   end
   
   def personal_best
-    @scores.max()
+    scores.max
   end
   
   def personal_top_three
-    @scores.sort().reverse().take(3)
+    scores.sort.reverse.take(3)
   end
   
   def latest_is_personal_best?

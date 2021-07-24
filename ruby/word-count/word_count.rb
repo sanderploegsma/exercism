51a66d4e6dc91bd8ef594b0f09c4c80206527734
Sander Ploegsma <sanderploegsma@gmail.com>
2021-07-24 11:26:42 +0200

    [Ruby] Word Count update

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -2,6 +2,6 @@ class Phrase
   attr_reader :word_count
 
   def initialize(phrase)
-    @word_count = phrase.downcase.scan(/\w+'\w+|\w+/).tally
+    @word_count = phrase.downcase.scan(/\b[\w']+\b/).tally
   end
 end

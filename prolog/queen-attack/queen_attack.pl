729bd9eb5a760c9e9eb667815ffaf2b48d28b8af
Sander Ploegsma <sander.ploegsma@topic.nl>
2023-01-19 17:23:13 +0100

    [Prolog] Sync with online IDE

---
+++ prolog/queen-attack/queen_attack.pl
@@ -0,0 +1,6 @@
+in_bounds(N) :- between(0, 7, N).
+create((X, Y)) :- in_bounds(X), in_bounds(Y).
+
+attack((X, _), (X, _)).
+attack((_, Y), (_, Y)).
+attack((X1, Y1), (X2, Y2)) :- abs(Y1 - Y2) =:= abs(X1 - X2).

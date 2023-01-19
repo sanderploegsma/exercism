0f1269e4fc5655e4433930c0d67c3212eb675305
Sander Ploegsma <sanderploegsma@gmail.com>
2023-01-19 22:40:44 +0100

    Update triangle.pl

--- prolog/triangle/triangle.pl
+++ prolog/triangle/triangle.pl
@@ -10,6 +10,6 @@ isosceles(A, A, _).
 isosceles(A, _, A).
 isosceles(_, A, A).
 
-triangle(A, A, A, "equilateral") :- triangle(A, A, A).
+triangle(A, A, A, "equilateral") :- triangle(A, A, A), !.
 triangle(A, B, C, "isosceles") :- triangle(A, B, C), isosceles(A, B, C), !.
 triangle(A, B, C, "scalene") :- triangle(A, B, C), A =\= B, A =\= C, B =\= C.

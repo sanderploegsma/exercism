09b5f01bf1f2fdac5f6d806511992d46141ced27
Sander Ploegsma <sanderploegsma@gmail.com>
2023-01-19 22:24:50 +0100

    Update test-all.sh

--- prolog/test-all.sh
+++ prolog/test-all.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-for file in **/*.pl; do
-    TARGET=$(dirname $file)
-    TARGET_LOWER="${TARGET/-/_}"
-    swipl -f "$TARGET/$TARGET_LOWER.pl" -s "$TARGET/${TARGET_LOWER}_tests.plt" -g run_tests -t halt
+set -euo pipefail
+
+declare -i TEST_RESULT=0
+FAILED_EXERCISES=''
+
+for file in $(find * -type f -name '*.pl')
+do
+    exercise_dir=$(dirname "${file}")
+    exercise=$(basename "${file}" .pl)
+    echo "Testing ${exercise}"
+    swipl -f "${exercise_dir}/${exercise}.pl" -s "${exercise_dir}/${exercise}_tests.plt" -g run_tests,halt -t 'halt(1)' -- --all
+    if [ $? -ne 0 ]; then
+        TEST_RESULT=1
+        FAILED_EXERCISES+="${exercise}\n"
+    fi
 done
+
+if [ "${TEST_RESULT}" -ne 0 ]; then
+    echo "The following exercises failed"
+    printf "${FAILED_EXERCISES}"
+    exit "${TEST_RESULT}"
+fi

66654a65f4f86866ff0b1b8103a14687759f9a71
Sander Ploegsma <sander.ploegsma@topic.nl>
2023-01-20 15:11:35 +0100

    [Prolog] Update test script

--- prolog/test-all.sh
+++ prolog/test-all.sh
@@ -7,18 +7,20 @@ FAILED_EXERCISES=''
 
 for file in $(find * -type f -name '*.pl')
 do
+    error=
     exercise_dir=$(dirname "${file}")
     exercise=$(basename "${file}" .pl)
     echo "Testing ${exercise}"
-    swipl -f "${exercise_dir}/${exercise}.pl" -s "${exercise_dir}/${exercise}_tests.plt" -g run_tests,halt -t 'halt(1)' -- --all
-    if [ $? -ne 0 ]; then
+    swipl -f "${exercise_dir}/${exercise}.pl" -s "${exercise_dir}/${exercise}_tests.plt" -g run_tests,halt -t 'halt(1)' -- --all || error=true
+    if [ $error ]; then
         TEST_RESULT=1
         FAILED_EXERCISES+="${exercise}\n"
     fi
 done
 
 if [ "${TEST_RESULT}" -ne 0 ]; then
-    echo "The following exercises failed"
+    echo "The following exercises failed:"
+    echo "-------------------------------"
     printf "${FAILED_EXERCISES}"
     exit "${TEST_RESULT}"
 fi

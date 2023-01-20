09888d1a38c3564f8ab2a2ff12e51cc3debf5064
Sander Ploegsma <sander.ploegsma@topic.nl>
2023-01-20 11:12:44 +0100

    [Python] Anagram update

--- prolog/anagram/anagram.pl
+++ prolog/anagram/anagram.pl
@@ -1,13 +1,9 @@
-chars_sorted(Str, Sorted) :-
-    string_chars(Str, Chars),
-    msort(Chars, Sorted).
-
 is_anagram(Word, Option) :-
     string_lower(Word, WordLowered),
     string_lower(Option, OptionLowered),
     WordLowered \= OptionLowered,
-    chars_sorted(WordLowered, WordChars),
-    chars_sorted(OptionLowered, OptionChars),
-    WordChars == OptionChars.
+    string_chars(WordLowered, WordChars),
+    string_chars(OptionLowered, OptionChars),
+    permutation(WordChars, OptionChars).
 
 anagram(Word, Options, Matching) :- include(is_anagram(Word), Options, Matching).

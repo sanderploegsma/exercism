bfab04cef49679be7641a5e01ad53b0d879b9c21
Sander Ploegsma <sanderploegsma@gmail.com>
2023-01-24 21:25:43 +0100

    [Go] Welcome To Tech Palace

---
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -0,0 +1,22 @@
+package techpalace
+
+import (
+	"fmt"
+	"strings"
+)
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	stars := strings.Repeat("*", numStarsPerLine)
+	return fmt.Sprintf("%s\n%s\n%s", stars, welcomeMsg, stars)
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+}

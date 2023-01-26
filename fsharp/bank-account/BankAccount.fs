2dda7c25c839d655d8abd7ec6b48cc92e845d153
Sander Ploegsma <sander.ploegsma@topic.nl>
2023-01-26 16:14:48 +0100

    [F#] Fix bank-account on .NET 7

--- fsharp/bank-account/BankAccount.fs
+++ fsharp/bank-account/BankAccount.fs
@@ -3,26 +3,26 @@
 open System
 open System.Collections.Concurrent
 
-let store = ConcurrentDictionary<Guid, Decimal>()
+let store = ConcurrentDictionary<Guid, decimal>()
 
 let mkBankAccount() = Guid.NewGuid()
 
-let openAccount account =
+let openAccount (account: Guid) =
     store.AddOrUpdate(account, 0m, (fun _ _ -> 0m)) |> ignore
     account
 
-let closeAccount account =
+let closeAccount (account: Guid) =
     store.TryRemove(account) |> ignore
     account
 
-let getBalance account =
+let getBalance (account: Guid) =
     let mutable balance = 0m
     if store.TryGetValue(account, &balance) then
         Some balance
     else
         None
 
-let updateBalance change account =
+let updateBalance change (account: Guid) =
     if store.ContainsKey(account) then
         store.AddOrUpdate(account, change, (fun _ balance -> balance + change)) |> ignore
     account

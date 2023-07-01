750f301fdefe96b42b333e4f97b592879b58f3d1
Sander Ploegsma <sanderploegsma@gmail.com>
2023-07-01 21:51:16 +0200

    [Ruby] Boutique Inventory Improvements

---
+++ ruby/boutique-inventory-improvements/boutique_inventory.rb
@@ -0,0 +1,17 @@
+require "ostruct"
+
+class BoutiqueInventory
+  attr_reader :items
+
+  def initialize(items)
+    @items = items.map { |item| OpenStruct.new(item) }
+  end
+
+  def item_names
+    items.map(&:name).sort
+  end
+
+  def total_stock
+    items.map(&:quantity_by_size).map(&:values).sum(&:sum)
+  end
+end

89e238fd9c940974de9f0384b9052496c266f362
Sander Ploegsma <sanderploegsma@gmail.com>
2023-07-01 13:14:28 +0200

    [Ruby] Amusement Park Improvements

---
+++ ruby/amusement-park-improvements/attendee.rb
@@ -0,0 +1,27 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  # Do not edit above methods, add your own methods below.
+
+  def has_pass?
+    @pass_id != nil
+  end
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    self.has_pass? && self.fits_ride?(ride_minimum_height)
+  end
+end

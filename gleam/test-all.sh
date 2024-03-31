f9998758c0d109139580609059fa35e82d2b37ef
Sander Ploegsma <sanderploegsma@gmail.com>
2024-03-31 21:18:40 +0200

    [Gleam] Set up GHA workflow

---
+++ gleam/test-all.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+trap "exit 1" SIGINT SIGTERM
+
+declare -i TEST_RESULT=0
+FAILED_EXERCISES=''
+
+function print_block {
+    printf "\n\n"
+    printf '=%.0s' {1..80}
+    printf "\n$1\n"
+    printf '=%.0s' {1..80}
+    printf "\n\n"
+}
+
+for file in $(find . -type f -name 'gleam.toml' -d 2)
+do
+    error=
+    exercise_dir=$(dirname "${file}")
+    exercise=${exercise_dir#./}
+    
+    print_block "${exercise}"
+
+    cd "${exercise_dir}"
+    gleam test || error=true
+    if [ $error ]; then
+        TEST_RESULT=1
+        FAILED_EXERCISES+="${exercise}\n"
+    fi
+    cd ..
+done
+
+if [ "${TEST_RESULT}" -ne 0 ]; then
+    print_block "The following exercises failed"
+    printf "${FAILED_EXERCISES}"
+    exit "${TEST_RESULT}"
+fi

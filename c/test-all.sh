8e9fdcbb809c071954893250a49dfb8200568837
Sander Ploegsma <sanderploegsma@gmail.com>
2025-01-27 21:00:59 +0100

    [C] Add Github Actions workflow

---
+++ c/test-all.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+trap "exit 1" SIGINT SIGTERM
+
+declare -i TEST_RESULT=0
+FAILED_EXERCISES=''
+
+function print_block {
+    printf "\n\n"
+    printf '=%.0s' {1..80}
+    printf "\n$1\n"
+    printf '=%.0s' {1..80}
+    printf "\n\n"
+}
+
+for file in $(find . -maxdepth 2 -type f -name 'makefile')
+do
+    error=
+    exercise_dir=$(dirname "${file}")
+    exercise=${exercise_dir#./}
+    
+    print_block "${exercise}"
+
+    cd "${exercise_dir}"
+    make test || error=true
+    if [ $error ]; then
+        TEST_RESULT=1
+        FAILED_EXERCISES+="${exercise}\n"
+    fi
+    cd ..
+done
+
+if [ "${TEST_RESULT}" -ne 0 ]; then
+    print_block "The following exercises failed"
+    printf "${FAILED_EXERCISES}"
+    exit "${TEST_RESULT}"
+fi

74848776c524478b4c284898c874246192989d1c
Sander Ploegsma <sanderploegsma@gmail.com>
2025-09-23 00:17:35 +0200

    [Rust] RNA Transcription v2

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,41 +1,22 @@
 use std::str::FromStr;
 
-macro_rules! dna_rna {
-    ($struct_name:ident, $enum_name:ident, $($enum_member:tt),+) => {
-        #[derive(Debug, PartialEq, Eq)]
-        pub enum $enum_name { $($enum_member),+ }
+use strum_macros::EnumString;
 
-        impl FromStr for $enum_name {
-            type Err = ();
-
-            fn from_str(s: &str) -> Result<$enum_name, ()> {
-                $(if s == stringify!($enum_member) { Ok($enum_name::$enum_member) } else)*
-                { Err(()) }
-            }
-        }
-
-        #[derive(Debug, PartialEq, Eq)]
-        pub struct $struct_name(Vec<$enum_name>);
-
-        impl FromIterator<$enum_name> for $struct_name {
-            fn from_iter<T: IntoIterator<Item = $enum_name>>(iter: T) -> Self {
-                $struct_name(iter.into_iter().collect())
-            }
-        }
-
-        impl $struct_name {
-            pub fn new(s: &str) -> Result<$struct_name, usize> {
-                s.chars()
-                    .enumerate()
-                    .map(|(i, c)| c.to_string().parse::<$enum_name>().map_err(|_| i))
-                    .collect()
-            }
-        }
-    };
+#[derive(Debug, PartialEq, Eq, EnumString)]
+pub enum DnaNucleotide {
+    A,
+    C,
+    G,
+    T,
 }
 
-dna_rna!(Dna, DnaNucleotide, A, C, G, T);
-dna_rna!(Rna, RnaNucleotide, A, C, G, U);
+#[derive(Debug, PartialEq, Eq, EnumString)]
+pub enum RnaNucleotide {
+    A,
+    C,
+    G,
+    U,
+}
 
 impl DnaNucleotide {
     fn transcribe(&self) -> RnaNucleotide {
@@ -48,8 +29,31 @@ impl DnaNucleotide {
     }
 }
 
+fn parse<T: FromStr>(s: &str) -> Result<Vec<T>, usize> {
+    s.chars()
+        .enumerate()
+        .map(|(i, c)| c.to_string().parse::<T>().map_err(|_| i))
+        .collect()
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Dna(Vec<DnaNucleotide>);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Rna(Vec<RnaNucleotide>);
+
 impl Dna {
+    pub fn new(dna: &str) -> Result<Dna, usize> {
+        parse(dna).map(Dna)
+    }
+
     pub fn into_rna(self) -> Rna {
         Rna(self.0.iter().map(|n| n.transcribe()).collect())
+    }
+}
+
+impl Rna {
+    pub fn new(rna: &str) -> Result<Rna, usize> {
+        parse(rna).map(Rna)
     }
 }
